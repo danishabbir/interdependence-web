8f8f28de6a6cac4e5e1f0ba860be7dd1abcc7a02
Daanish <danishabbir@users.noreply.github.com>
2022-01-18 13:50:54 +0500

    redirect to sigeconomies

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/',
-        destination: '/about',
+        destination: '/signatureEconomies',
         permanent: true,
       }
     ];
