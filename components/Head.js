1b25bfd3584f69b69d4b15a5bbf62d4dd61dcd75
Daanish <danishabbir@users.noreply.github.com>
2022-01-18 13:53:59 +0500

    website title

--- components/Head.js
+++ components/Head.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 export default function HeadComponent() {
   return <Head>
-    <title>A Declaration of the Interdependence of Cyberspace</title>
+    <title>Signature Economies</title>
     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,600;1,400&family=Roboto+Mono&display=swap" rel="stylesheet"/>
     
     {/* Place any emoji as text wrapped in svg for favicon */}
